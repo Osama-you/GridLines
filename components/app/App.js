b2d1ea3d9558f806f72d93178737220bf38ae180
Osama-you <Osama.W.Younis@gmail.com>
2019-10-11 10:00:37 +0300

    Start Over

--- components/app/App.js
+++ components/app/App.js
@@ -8,26 +8,7 @@ class App{
     run(){
         // get the body
         const root = document.querySelector("#root");
-        // set the coloer for body
-        root.style.backgroundColor = "#232327";
-
-        // set a SideBar
-        const sidebar = new SideBar();
-        // same setting for SideBar
-        sidebar.setBackgroundColor("#3C3552");
-        sidebar.setInitWidth("10px");
-        sidebar.setHeight("86vh");
-        sidebar.setSpeed(0.5);
-        sidebar.setParent(root);
-        // show the SideBar
-        sidebar.display();
-        // console.log(root.clientWidth)
-        // root.style.margin = sidebar.getInitWidth();
-
-        const DS = new DrawingSpace();
-        DS.setParent(root);
-        DS.display();
-
+        
     }
 }
 
--- components/drawingSpace/DrawingSpace.js
+++ components/drawingSpace/DrawingSpace.js
@@ -6,7 +6,7 @@ class DrawingSpace{
            display: grid;` 
     };
     theAction = 0;
-    constructor(pixelWidth = 22 ,pixelHeight= 22 ,parent) {
+    constructor(pixelWidth = 50 ,pixelHeight= 50 ,parent) {
         this.drawingSpace = document.createElement("div");
         this.pixelWidth = pixelWidth;
         this.pixelHeight = pixelHeight;
@@ -44,12 +44,13 @@ class DrawingSpace{
     
     fillThaPixel(thatAction){
         if(theAction === thatAction){
-            
+            // this.style.backgroundColor = "red";
+            console.log("adas");
         }else if(theAction === thatAction){
-
+            this.style.backgroundColor = "red";
+            console.log("adas");
         }
     }
-
 
     display(){
         this.parent.appendChild(this.drawingSpace);
@@ -58,36 +59,82 @@ class DrawingSpace{
         this.drawingSpace.style = this.style.drawingSpace;
         const theWidth =  this.drawingSpace.clientWidth;
         const theHeight =  this.drawingSpace.clientHeight;
-        
+        console.log('theWidth'+theWidth);
+        console.log('theHeight'+theHeight);
         const  pixelH_No = Math.floor(theHeight/this.pixelHeight);
         const pixelW_No= Math.floor(theWidth/this.pixelWidth);
-        
-        const marginTB = (((theHeight%this.pixelHeight)/2)/theHeight)*100; // vh
-        const marginLF = (theWidth%this.pixelWidth)/2;  // px
-        const newHeight = 100 - (marginTB*2) ;  // vh
+        console.log('pixelH_No'+pixelH_No);
+        console.log('pixelW_No'+pixelW_No);
+        // const spaTB = (((theHeight%this.pixelHeight)/2)/theHeight)*100; // vh
+        // console.log('spaTB'+spaTB);
 
+        const spaTB = theHeight-(pixelH_No*this.pixelHeight);
+
+
+        const marginLF = (theWidth%this.pixelWidth)/2;  // px
+        console.log('marginLF'+marginLF);
+        const newHeight = 100-0.254 - (spaTB*0.24963*2) ;  // vh
+        console.log('newHeight'+newHeight);
 
         this.drawingSpace.style.gridTemplateColumns= `repeat(${pixelW_No}, ${this.pixelHeight}px)`;
         this.drawingSpace.style.gridTemplateRows= `repeat(${pixelH_No}, ${this.pixelWidth}px)`;
         
         let i = 0;
-        
-        while (i<(pixelH_No*pixelW_No)) {
-            let timp = document.createElement("div");
-            timp.style.width=this.pixelWidth;
-            timp.style.height=this.pixelHeight;
-            timp.style.backgroundColor="red";
-            
-            timp.addEventListener("mouseover", fillThaPixel(0));
 
-            timp.addEventListener("click", fillThaPixel(1));
-           
-            this.drawingSpace.appendChild(timp);
+        
+        while (i<(pixelH_No*pixelW_No+1)) {
+            if(i<pixelH_No*pixelW_No){
+                let timp = document.createElement("div");
+                timp.style.width=this.pixelWidth;
+                timp.style.height=this.pixelHeight;
+                // timp.style.backgroundColor="red";
+                var coordsQueue = [];
+    
+                window.addEventListener('mousemove', function(e){
+                    coordsQueue.push([e.pageX, e.pageY]);
+                    
+                })
+                // console.log(coordsQueue);
+                timp.addEventListener("mousemove",()=>{
+                    timp.style.backgroundColor="red";
+                });
+    
+                timp.addEventListener("click",()=>{
+                    timp.style.backgroundColor="red";
+                    console.log(this.theAction);
+                });
+               
+                this.drawingSpace.appendChild(timp);
+            }else{
+                let timp = document.createElement("div");
+                timp.style.width=this.pixelWidth;
+                timp.style.height=spaTB;
+                // timp.style.backgroundColor="red";
+                var coordsQueue = [];
+    
+                window.addEventListener('mousemove', function(e){
+                    coordsQueue.push([e.pageX, e.pageY]);
+                    
+                })
+                // console.log(coordsQueue);
+                timp.addEventListener("mousemove",()=>{
+                    timp.style.backgroundColor="red";
+                });
+    
+                timp.addEventListener("click",()=>{
+                    timp.style.backgroundColor="red";
+                    console.log(this.theAction);
+                });
+               
+                this.drawingSpace.appendChild(timp);
+            }
+            
             i++;
         }
         this.drawingSpace.style.height = `${newHeight}vh`;
-       
-        this.drawingSpace.style.margin =`${marginTB}vh ${marginLF}px`;
+        // this.drawingSpace.style.margin =`${spaTB*0.24963*4}vh ${0.5+marginLF}px 0 ${0.5+marginLF}px`;
+
+        // this.drawingSpace.style.margin =`auto auto`;
         
     }
 
